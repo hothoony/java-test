f8db7c7c0580d2a0a3ea8a8fce018b71a2465e29
hothoony <hothoony@hanmail.net>
2020-11-09 13:00:56 +0900

    .

---
+++ profile-test/src/main/java/study/profiletest/ProfileTestApplication.java
@@ -0,0 +1,32 @@
+package study.profiletest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+//@Profile("prod")
+@SpringBootApplication
+public class ProfileTestApplication {
+
+    @Value("${my.server.url}")
+    private String myServerUrl;
+
+    @Value("${my.server.port}")
+    private int myServerPort;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileTestApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("myServerUrl = " + myServerUrl);
+            System.out.println("myServerPort = " + myServerPort);
+        };
+    }
+
+}
