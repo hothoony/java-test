053dd80d39699507db88b7b5ebfa6edef1a6852e
hothoony <hothoony@hanmail.net>
2020-11-17 16:48:51 +0900

    .

---
+++ stream-test/src/test/java/com/example/streamtest/streamTest/test_map_flatmap.java
@@ -0,0 +1,23 @@
+package com.example.streamtest.streamTest;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class test_map_flatmap {
+
+    @Test
+    void map() {
+        Integer[][] list = {{1, 2}, {3, 4}};
+        Stream<Stream<Integer>> stream = Arrays.stream(list).map(x -> Arrays.stream(x));
+        stream.forEach(s -> s.forEach(System.out::println));
+    }
+
+    @Test
+    void flatmap() {
+        Integer[][] list = {{1, 2}, {3, 4}};
+        Stream<Integer> stream = Arrays.stream(list).flatMap(x -> Arrays.stream(x));
+        stream.forEach(System.out::println);
+    }
+}
