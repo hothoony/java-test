cdcee9260a4236d54ae0d9bdc71f16da1e057379
hothoony <hothoony@hanmail.net>
2020-11-19 15:53:46 +0900

    .

---
+++ spring-boot-logback-test/src/main/java/study/springbootlogbacktest/SpringBootLogbackTestApplication.java
@@ -0,0 +1,13 @@
+package study.springbootlogbacktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoggingTestApplication.class, args);
+    }
+
+}
