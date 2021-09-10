04ee688b42eca9371a452d1d0cab7289845cd7b8
hothoony <hothoony@hanmail.net>
2021-09-10 11:46:27 +0900

    .

--- stream-test/src/main/java/com/example/streamtest/dto/MemberDto.java
+++ stream-test/src/main/java/com/example/streamtest/dto/MemberDto.java
@@ -7,10 +7,21 @@ public class MemberDto {
     
     private int id;
     private String name;
+    private String role;
 
     public MemberDto(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public MemberDto(int id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+    
+    public static MemberDto of(int id, String name, String role) {
+        return new MemberDto(id, name, role);
     }
 
     @Override
@@ -18,6 +29,7 @@ public class MemberDto {
         return "MemberDto{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", role='" + role + '\'' +
                 '}';
     }
 }
--- stream-test/src/test/java/com/example/streamtest/streamTest/test_all.java
+++ stream-test/src/test/java/com/example/streamtest/streamTest/test_all.java
@@ -1,15 +1,25 @@
 package com.example.streamtest.streamTest;
 
+import com.example.streamtest.dto.MemberDto;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class test_all {
     
     List<Integer> list = Arrays.asList(3, 2, 1, 5, 4);
+
+    List<MemberDto> members = Arrays.asList(
+            MemberDto.of(1, "memberA", "admin"),
+            MemberDto.of(2, "memberB", "staff"),
+            MemberDto.of(3, "memberC", "admin"),
+            MemberDto.of(4, "memberD", "staff"),
+            MemberDto.of(5, "memberE", "staff")
+    );
 
     @Test
     void count() {
@@ -30,5 +40,16 @@ public class test_all {
     void sum() {
         Integer sum = list.stream().reduce(0, Integer::sum);
         System.out.println("sum = " + sum);
+    }
+
+    @Test
+    void filter() {
+        List<MemberDto> adminMembers = members.stream()
+                .filter(o -> "admin".equals(o.getRole()))
+                .collect(Collectors.toList());
+
+        for (MemberDto adminMember : adminMembers) {
+            System.out.println("adminMember = " + adminMember);
+        }
     }
 }
