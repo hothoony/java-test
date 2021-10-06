9ee23735fb0412bea4cd5e59f33ec23a975f1205
hothoony <hothoony@hanmail.net>
2021-10-06 10:02:32 +0900

    .

--- javax-validation-test/src/test/java/util/DateUtil.java
+++ javax-validation-test/src/test/java/util/DateUtil.java
@@ -5,22 +5,14 @@ import java.time.LocalDate;
 public class DateUtil {
 
     public static LocalDate getYesterday() {
-        return getLocalDate(-1);
+        return LocalDate.now().plusDays(-1);
     }
 
     public static LocalDate getToday() {
-        return getLocalDate(0);
+        return LocalDate.now().plusDays(0);
     }
 
     public static LocalDate getTomorrow() {
-        return getLocalDate(1);
-    }
-
-    private static LocalDate getLocalDate(int dateDiff) {
-        LocalDate today = LocalDate.now();
-        return LocalDate.of(
-                today.getYear(),
-                today.getMonthValue(),
-                today.getDayOfMonth() + dateDiff);
+        return LocalDate.now().plusDays(1);
     }
 }
