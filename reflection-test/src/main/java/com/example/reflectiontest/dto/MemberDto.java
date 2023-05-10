0b5e69bf71f88da19eb59bb7cea3d342b7ad79f1
hothoony <hothoony@hanmail.net>
2023-05-10 23:29:33 +0900

    .

--- reflection-test/src/main/java/com/example/reflectiontest/dto/MemberDto.java
+++ reflection-test/src/main/java/com/example/reflectiontest/dto/MemberDto.java
@@ -10,4 +10,13 @@ public class MemberDto extends WebDto {
     private Long id;
     public String name;
     protected Integer age;
+
+    public MemberDto() {
+    }
+
+    public MemberDto(Long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 }
