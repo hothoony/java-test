1d6c2f455531baa8f67afb035a482f578109c3b7
hothoony <hothoony@hanmail.net>
2023-05-11 22:59:47 +0900

    .

--- generic-test/src/test/java/study/generictest/test/GenericClazzTest.java
+++ generic-test/src/test/java/study/generictest/test/GenericClazzTest.java
@@ -5,12 +5,14 @@ import study.generictest.test.code.generic.GenericClazz;
 import study.generictest.test.code.dto.MemberDto;
 import study.generictest.test.code.dto.TeamDto;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GenericClazzTest {
 
     @Test
-    void teamDto() {
+    void getDto_teamDto() {
         GenericClazz<TeamDto> genericClazz = new GenericClazz<>(); // HERE
         genericClazz.setDto(new TeamDto());
 
@@ -22,7 +24,7 @@ class GenericClazzTest {
     }
 
     @Test
-    void memberDto() {
+    void getDto_memberDto() {
         GenericClazz<MemberDto> genericClazz = new GenericClazz<>(); // HERE
         genericClazz.setDto(new MemberDto());
 
@@ -31,5 +33,23 @@ class GenericClazzTest {
         assertThat(dto.getClass() == MemberDto.class);
         System.out.println("dto.getClass() = " + dto.getClass());
         System.out.println("dto = " + dto);
+    }
+
+    @Test
+    void getDtoList_teamDto() {
+        GenericClazz<TeamDto> genericClazz = new GenericClazz<>();
+        genericClazz.setDtoList(new TeamDto());
+
+        List<TeamDto> list = genericClazz.getDtoList();
+        System.out.println("list = " + list);
+    }
+
+    @Test
+    void getDtoList_memberDto() {
+        GenericClazz<MemberDto> genericClazz = new GenericClazz<>();
+        genericClazz.setDtoList(new MemberDto());
+
+        List<MemberDto> list = genericClazz.getDtoList();
+        System.out.println("list = " + list);
     }
 }
--- generic-test/src/test/java/study/generictest/test/code/generic/GenericClazz.java
+++ generic-test/src/test/java/study/generictest/test/code/generic/GenericClazz.java
@@ -1,8 +1,12 @@
 package study.generictest.test.code.generic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericClazz<T> { // HERE
     
     private T dto; // HERE
+    private List<T> dtoList = new ArrayList<>();
 
     public void setDto(T dto) { // HERE
         this.dto = dto;
@@ -10,5 +14,13 @@ public class GenericClazz<T> { // HERE
 
     public T getDto() { // HERE
         return dto;
+    }
+
+    public void setDtoList(T dto) {
+        dtoList.add(dto);
+    }
+    
+    public List<T> getDtoList() {
+        return dtoList;
     }
 }
