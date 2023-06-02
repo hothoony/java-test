7c16da8d6a4ee14f2c614ef6a16c8500602c11e3
hothoony <hothoony@hanmail.net>
2023-06-03 01:28:57 +0900

    .

---
+++ random-password-test/src/main/java/com/example/randompasswordtest/RandomPasswordTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.randompasswordtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPasswordTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomPasswordTestApplication.class, args);
+    }
+
+}
---
+++ random-password-test/src/test/java/com/example/randompasswordtest/RandomPasswordTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randompasswordtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomPasswordTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
