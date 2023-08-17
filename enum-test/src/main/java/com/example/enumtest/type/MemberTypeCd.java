0887d766bdd9fa5c51c062f707adc7f11c3abe55
hothoony <hothoony@hanmail.net>
2023-08-17 10:21:35 +0900

    .

--- enum-test/src/main/java/com/example/enumtest/type/MemberTypeCd.java
+++ enum-test/src/main/java/com/example/enumtest/type/MemberTypeCd.java
@@ -6,7 +6,9 @@ public enum MemberTypeCd {
     MEMBER("20", "member"),
     
     MA0("MA0", "person"),
-    MB0("MB0", "company");
+    MB0("MB0", "company"),
+
+    NONE(null, "NO_DATA");
 
     private String code;
     private String name;
@@ -24,14 +26,31 @@ public enum MemberTypeCd {
         return name;
     }
 
-    public static MemberTypeCd ofCode(Object codeObj) {
-        String code = String.valueOf(codeObj);
-        if (ADMIN.code.equals(code)) {
-            return ADMIN;
+    public static MemberTypeCd ofCode(String code) {
+        try {
+            MemberTypeCd[] types = MemberTypeCd.values();
+            for (MemberTypeCd type : types) {
+                if (type.cd().equals(code)) {
+                    return type;
+                }
+            }
+            throw new IllegalStateException();
+        } catch (IllegalStateException e) {
+            return null;
         }
-        else if (MEMBER.code.equals(code)) {
-            return MEMBER;
+    }
+
+    public static MemberTypeCd ofCode2(String code) {
+        try {
+            MemberTypeCd[] types = MemberTypeCd.values();
+            for (MemberTypeCd type : types) {
+                if (type.cd() != null && type.cd().equals(code)) {
+                    return type;
+                }
+            }
+            throw new IllegalStateException();
+        } catch (IllegalStateException e) {
+            return MemberTypeCd.NONE;
         }
-        return null;
     }
 }
--- enum-test/src/test/java/com/example/enumtest/type/MemberTypeCdTest.java
+++ enum-test/src/test/java/com/example/enumtest/type/MemberTypeCdTest.java
@@ -3,14 +3,9 @@ package com.example.enumtest.type;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MemberTypeCdTest {
-
-    @Test
-    void ofCode_test() {
-        assertThat(MemberTypeCd.ofCode("10")).isEqualTo(MemberTypeCd.ADMIN);
-        assertThat(MemberTypeCd.ofCode("20")).isEqualTo(MemberTypeCd.MEMBER);
-    }
 
     @Test
     void test1() {
@@ -34,5 +29,54 @@ class MemberTypeCdTest {
         System.out.println(MemberTypeCd.MA0.toString());
         System.out.println(MemberTypeCd.MA0.cd());
         System.out.println(MemberTypeCd.MA0.nm());
+    }
+
+    @Test
+    void values_test() {
+        MemberTypeCd[] values = MemberTypeCd.values();
+        for (MemberTypeCd value : values) {
+            System.out.println("value = " + value);
+        }
+    }
+
+    @Test
+    void valueOf_exist_test() {
+        MemberTypeCd memberTypeCd = MemberTypeCd.valueOf("MEMBER");
+        System.out.println("memberTypeCd = " + memberTypeCd);
+    }
+
+    @Test
+    void valueOf_not_exist_test() {
+        
+        assertThatThrownBy(() -> {
+            MemberTypeCd memberTypeCd = MemberTypeCd.valueOf("AA"); // java.lang.IllegalArgumentException
+            System.out.println("memberTypeCd = " + memberTypeCd);
+        })
+        .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void ofCode_test() {
+        assertThat(MemberTypeCd.ofCode("10")).isEqualTo(MemberTypeCd.ADMIN);
+        assertThat(MemberTypeCd.ofCode("20")).isEqualTo(MemberTypeCd.MEMBER);
+        assertThat(MemberTypeCd.ofCode("MA0")).isEqualTo(MemberTypeCd.MA0);
+        assertThat(MemberTypeCd.ofCode("MB0")).isEqualTo(MemberTypeCd.MB0);
+        assertThat(MemberTypeCd.ofCode("AA")).isNull();
+        assertThat(MemberTypeCd.ofCode(null)).isNull();
+    }
+    @Test
+    void ofCode2_test() {
+        assertThat(MemberTypeCd.ofCode2("10")).isEqualTo(MemberTypeCd.ADMIN);
+        assertThat(MemberTypeCd.ofCode2("20")).isEqualTo(MemberTypeCd.MEMBER);
+        assertThat(MemberTypeCd.ofCode2("MA0")).isEqualTo(MemberTypeCd.MA0);
+        assertThat(MemberTypeCd.ofCode2("MB0")).isEqualTo(MemberTypeCd.MB0);
+        assertThat(MemberTypeCd.ofCode2("AA")).isEqualTo(MemberTypeCd.NONE);
+        assertThat(MemberTypeCd.ofCode2(null)).isEqualTo(MemberTypeCd.NONE);
+    }
+
+    @Test
+    void none_cd_is_null() {
+        MemberTypeCd memberTypeCd = MemberTypeCd.NONE;
+        assertThat(memberTypeCd.cd()).isNull();
     }
 }
