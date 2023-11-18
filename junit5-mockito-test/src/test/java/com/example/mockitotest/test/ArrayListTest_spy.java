efdde8a61b394bde81fc66d2ae6ea5b46e38fb04
hothoony <hothoony@hanmail.net>
2023-11-18 11:32:16 +0900

    .

--- junit5-mockito-test/src/test/java/com/example/mockitotest/test/ArrayListTest_spy.java
+++ junit5-mockito-test/src/test/java/com/example/mockitotest/test/ArrayListTest_spy.java
@@ -2,7 +2,6 @@ package com.example.mockitotest.test;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
