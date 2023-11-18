b8ca03040ecf9154d20714b8543498818ee670f9
hothoony <hothoony@hanmail.net>
2023-11-18 12:13:06 +0900

    .

--- junit5-mockito-test/src/test/java/com/example/mockitotest/service/MemberServiceTest_saveMember_mock.java
+++ junit5-mockito-test/src/test/java/com/example/mockitotest/service/MemberServiceTest_saveMember_mock.java
@@ -4,6 +4,7 @@ import com.example.mockitotest.domain.Member;
 import com.example.mockitotest.dto.MemberAgreeDto;
 import com.example.mockitotest.dto.ReqMemberAddDto;
 import com.example.mockitotest.repository.MemberRepository;
+import org.assertj.core.api.AbstractThrowableAssert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,9 +14,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.ui.ModelMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest_saveMember_mock {
@@ -32,25 +35,38 @@ class MemberServiceTest_saveMember_mock {
     @Mock
     ModelMapper modelMapper;
 
+    @Mock
+    MailService mailService;
+
+    @Mock
+    SmsService smsService;
+
     @Test
-    void notNull() {
-        assertThat(memberService).isNotNull();
+    void saveMember_exception() {
+
+        AbstractThrowableAssert<?, ? extends Throwable> abstractThrowableAssert = assertThatThrownBy(() -> {
+
+            given(modelMapper.map(any(), eq(Member.class))).willReturn(new Member("memberA"));
+            given(modelMapper.map(any(), eq(MemberAgreeDto.class))).willReturn(new MemberAgreeDto());
+            given(memberRepository.save(any())).willReturn(new Member("memberA"));
+            given(memberAgreeService.saveMemberAgree(any(MemberAgreeDto.class)))
+                    .willThrow(new RuntimeException("테스트 예외"));
+
+            ReqMemberAddDto reqMemberAddDto = new ReqMemberAddDto("memberA");
+            reqMemberAddDto.setMemberId(1L);
+            Member member = new Member(1L, "memberA");
+
+            Member actual = memberService.saveMember(reqMemberAddDto);
+
+        });
+
+        abstractThrowableAssert.isInstanceOf(RuntimeException.class);
+        abstractThrowableAssert.hasMessage("테스트 예외");
+
     }
 
     @Test
-    void saveMember() {
-
-        given(modelMapper.map(any(), Member.class)).willReturn(new Member(eq("memberA")));
-        given(modelMapper.map(any(), ReqMemberAddDto.class)).willReturn(new ReqMemberAddDto(eq("memberA")));
-        given(memberAgreeService.saveMemberAgree(any(MemberAgreeDto.class)))
-                .willThrow(new RuntimeException("테스트 예외"));
-
-        ReqMemberAddDto reqMemberAddDto = new ReqMemberAddDto("memberA");
-        reqMemberAddDto.setMemberId(1L);
-        Member member = new Member(1L, "memberA");
-
-        Member actual = memberService.saveMember(reqMemberAddDto);
-
-        assertThat(actual).isEqualTo(member);
+    void notNull() {
+        assertThat(memberService).isNotNull();
     }
 }
