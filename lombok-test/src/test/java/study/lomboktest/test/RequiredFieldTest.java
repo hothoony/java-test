f291d3633af14344a6c1d1c3b42868d06b4e9c5e
hothoony <hothoony@hanmail.net>
2023-11-26 20:21:59 +0900

    .

---
+++ lombok-test/src/test/java/study/lomboktest/test/RequiredFieldTest.java
@@ -0,0 +1,48 @@
+package study.lomboktest.test;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import org.junit.jupiter.api.Test;
+
+public class RequiredFieldTest {
+
+    @Test
+    void test1() {
+
+        Person person = Person.builder()
+                .name("james")
+                .build();
+        System.out.println("person = " + person);
+
+    }
+
+    @Test
+    void test2() {
+
+        Company company = Company.builder()
+                .build();
+        System.out.println("company = " + company);
+    }
+
+    @Builder
+    @Data
+    static class Person {
+
+        @NonNull
+        private String name;
+        private Integer age;
+    }
+
+    @Data
+    static class Company {
+
+        private String name;
+        private Integer brno;
+
+        @Builder
+        public Company(String name) {
+            this.name = name;
+        }
+    }
+}
