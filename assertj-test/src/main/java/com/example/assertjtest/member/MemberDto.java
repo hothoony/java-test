d60c9aca377aaa6497509a28bbcdd344ac2573cc
hothoony <hothoony@hanmail.net>
2023-11-28 16:14:38 +0900

    .

--- assertj-test/src/main/java/com/example/assertjtest/member/MemberDto.java
+++ assertj-test/src/main/java/com/example/assertjtest/member/MemberDto.java
@@ -1,4 +1,14 @@
 package com.example.assertjtest.member;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class MemberDto {
+    
+    private Integer memberSeq;
+    private String memberNm;
+    private String memberEmail;
+    private Integer age;
 }
---
+++ assertj-test/src/test/java/com/example/assertjtest/list_extracting_test.java
@@ -0,0 +1,43 @@
+package com.example.assertjtest;
+
+import com.example.assertjtest.member.MemberDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class list_extracting_test {
+
+
+    @Test
+    void test1() {
+
+        List<MemberDto> memberDtos = Arrays.asList(
+                MemberDto.builder().memberSeq(1).memberNm("memberA").age(10).build(),
+                MemberDto.builder().memberSeq(2).memberNm("memberD").age(50).build(),
+                MemberDto.builder().memberSeq(3).memberNm("memberC").age(20).build(),
+                MemberDto.builder().memberSeq(4).memberNm("memberE").age(40).build(),
+                MemberDto.builder().memberSeq(5).memberNm("memberB").age(30).build()
+        );
+
+        assertThat(memberDtos)
+                .isNotNull()
+                .hasSize(5)
+        ;
+
+        assertThat(memberDtos)
+                .extracting("memberSeq").containsExactly(1, 2, 3, 4, 5)
+        ;
+
+        assertThat(memberDtos)
+                .extracting("age").containsExactly(10, 50, 20, 40, 30)
+        ;
+
+        assertThat(memberDtos)
+                .extracting("age").containsExactlyInAnyOrder(10, 20, 30, 40, 50)
+        ;
+    }
+}
