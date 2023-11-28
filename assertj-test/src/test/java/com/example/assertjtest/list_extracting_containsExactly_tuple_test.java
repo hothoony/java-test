f0bcb382bc75ace8de7c8cd5026ec7123c9c6d0c
hothoony <hothoony@hanmail.net>
2023-11-28 16:36:21 +0900

    .

--- assertj-test/src/test/java/com/example/assertjtest/list_extracting_containsExactly_tuple_test.java
+++ assertj-test/src/test/java/com/example/assertjtest/list_extracting_containsExactly_tuple_test.java
@@ -12,32 +12,27 @@ public class list_extracting_containsExactly_tuple_test {
 
 
     @Test
-    void test1() {
+    void column1_test() {
 
-        List<MemberDto> memberDtos = Arrays.asList(
-                MemberDto.builder().memberSeq(1).memberNm("memberA").age(10).build(),
-                MemberDto.builder().memberSeq(2).memberNm("memberD").age(50).build(),
-                MemberDto.builder().memberSeq(3).memberNm("memberC").age(20).build(),
-                MemberDto.builder().memberSeq(4).memberNm("memberE").age(40).build(),
-                MemberDto.builder().memberSeq(5).memberNm("memberB").age(30).build()
-        );
+        List<MemberDto> memberDtos = getMemberDtos();
 
         assertThat(memberDtos)
-                .isNotNull()
-                .hasSize(5)
-        ;
+                .extracting("memberSeq").contains(1, 2, 3);
+        
+        assertThat(memberDtos)
+                .extracting("memberSeq").containsExactly(1, 2, 3, 4, 5);
 
         assertThat(memberDtos)
-                .extracting("memberSeq").containsExactly(1, 2, 3, 4, 5)
-        ;
+                .extracting("memberSeq").containsAnyOf(3, 4, 5);
 
         assertThat(memberDtos)
-                .extracting("age").containsExactly(10, 50, 20, 40, 30)
-        ;
+                .extracting("memberSeq").containsExactlyInAnyOrder(5, 2, 3, 4, 1);
+    }
 
-        assertThat(memberDtos)
-                .extracting("age").containsExactlyInAnyOrder(10, 20, 30, 40, 50)
-        ;
+    @Test
+    void column2_test() {
+
+        List<MemberDto> memberDtos = getMemberDtos();
 
         assertThat(memberDtos)
                 .extracting("memberSeq", "age")
@@ -49,5 +44,36 @@ public class list_extracting_containsExactly_tuple_test {
                         tuple(5, 30)
                 )
         ;
+
+        assertThat(memberDtos)
+                .extracting("memberSeq", "age")
+                .containsExactlyInAnyOrder(
+                        tuple(2, 50),
+                        tuple(1, 10),
+                        tuple(3, 20),
+                        tuple(4, 40),
+                        tuple(5, 30)
+                )
+        ;
+
+        assertThat(memberDtos)
+                .extracting("memberSeq", "age")
+                .containsAnyOf(
+                        tuple(2, 50),
+                        tuple(4, 40),
+                        tuple(5, 30)
+                )
+        ;
+    }
+
+    private static List<MemberDto> getMemberDtos() {
+        List<MemberDto> memberDtos = Arrays.asList(
+                MemberDto.builder().memberSeq(1).memberNm("memberA").age(10).build(),
+                MemberDto.builder().memberSeq(2).memberNm("memberD").age(50).build(),
+                MemberDto.builder().memberSeq(3).memberNm("memberC").age(20).build(),
+                MemberDto.builder().memberSeq(4).memberNm("memberE").age(40).build(),
+                MemberDto.builder().memberSeq(5).memberNm("memberB").age(30).build()
+        );
+        return memberDtos;
     }
 }
