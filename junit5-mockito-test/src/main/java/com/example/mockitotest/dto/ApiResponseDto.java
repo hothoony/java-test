e8b21094132987174cd8bb692018f95dce6f5c7a
hothoony <hothoony@hanmail.net>
2023-12-28 21:13:19 +0900

    .

--- junit5-mockito-test/src/main/java/com/example/mockitotest/dto/ApiResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.mockitotest.dto;
-
-import lombok.Data;
-
-@Data
-public class ApiResponseDto {
-    
-    
-}
--- junit5-mockito-test/src/main/java/com/example/mockitotest/repository/MemberAgreeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mockitotest.repository;
-
-import com.example.mockitotest.domain.MemberAgree;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberAgreeRepository extends JpaRepository<MemberAgree, Long> {
-    
-}
