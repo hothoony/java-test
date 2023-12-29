e157243088eea958b4edba9139bb6c0a651ec8b4
hothoony <hothoony@hanmail.net>
2023-12-29 14:51:55 +0900

    .

--- thread-test/src/test/java/study/threadtest/threadPool/ExecutorServiceTest.java
+++ thread-test/src/test/java/study/threadtest/threadPool/ExecutorServiceTest.java
@@ -2,23 +2,36 @@ package study.threadtest.threadPool;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ExecutorServiceTest {
 
     @Test
-    void test1() {
+    void test1() throws InterruptedException {
 
-        int threadCount = 2;
+        int threadCount = 32;
 
         ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
         System.out.println("executorService = " + executorService);
 
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
         for (int i = 0; i < threadCount; i++) {
-            executorService.execute(this::serviceJob);
+            executorService.execute(() -> {
+                try {
+                    String threadName = Thread.currentThread().getName();
+                    System.out.println(threadName + " => 1. DB 데이터 조회");
+                    System.out.println(threadName + " => 2. 객체 데이터 업데이트");
+                    System.out.println(threadName + " => 3. DB 데이터 업데이트");
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
         }
+
+        countDownLatch.await();
 
         System.out.println("executorService.isShutdown() = " + executorService.isShutdown());
         System.out.println("executorService.isTerminated() = " + executorService.isTerminated());
@@ -27,10 +40,4 @@ public class ExecutorServiceTest {
         System.out.println("executorService.isTerminated() = " + executorService.isTerminated());
     }
 
-    private void serviceJob() {
-        String threadName = Thread.currentThread().getName();
-        System.out.println(threadName + " => 1. DB 데이터 조회");
-        System.out.println(threadName + " => 2. 객체 데이터 업데이트");
-        System.out.println(threadName + " => 3. DB 데이터 업데이트");
-    }
 }
