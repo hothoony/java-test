07bff3b91dea49862f4563831d0b1b67b2686839
hothoony <hothoony@hanmail.net>
2024-02-01 16:04:13 +0900

    .

--- LocalDateTime-test/src/test/java/study/localdatetimetest/statDateTest_day_week_month_year.java
+++ LocalDateTime-test/src/test/java/study/localdatetimetest/statDateTest_day_week_month_year.java
@@ -1,22 +1,41 @@
 package study.localdatetimetest;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 
 public class statDateTest_day_week_month_year {
     
-    // day, week, month, year
-
+    @DisplayName("localDate 로 일,주,월,년 구하기")
     @Test
-    void day() {
+    void test1() {
 
-        LocalDateTime beginDt = LocalDateTime.now();
-        System.out.println("beginDt = " + beginDt);
+        // day, week, month, year
 
-        LocalDateTime truncSecond = beginDt.truncatedTo(ChronoUnit.SECONDS);
-        System.out.println("truncSecond = " + truncSecond);
+//        LocalDate localDate = LocalDate.now();
+        LocalDate localDate = LocalDate.parse("20230506", DateTimeFormatter.ofPattern("yyyyMMdd"));
+        System.out.println("localDate = " + localDate);
+
+        LocalDate weekBgn = localDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
+        LocalDate weekEnd = localDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY));
+        System.out.println();
+        System.out.println("weekBgn   = " + weekBgn);
+        System.out.println("weekEnd   = " + weekEnd);
+
+        LocalDate monthBgn = localDate.with(TemporalAdjusters.firstDayOfMonth());
+        LocalDate monthEnd = localDate.with(TemporalAdjusters.lastDayOfMonth());
+        System.out.println();
+        System.out.println("monthBgn  = " + monthBgn);
+        System.out.println("monthEnd  = " + monthEnd);
+
+        LocalDate yearBgn = localDate.with(TemporalAdjusters.firstDayOfYear());
+        LocalDate yearEnd = localDate.with(TemporalAdjusters.lastDayOfYear());
+        System.out.println();
+        System.out.println("yearBgn   = " + yearBgn);
+        System.out.println("yearEnd   = " + yearEnd);
     }
 }
