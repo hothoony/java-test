e341d3bf8659b96762baefdbf8f58aa68958a0d8
hothoony <hothoony@hanmail.net>
2024-10-11 23:38:14 +0900

    .

---
+++ _code-snippet-test/Test_basic.java
@@ -0,0 +1,21 @@
+
+
+public class Test_basic {
+    
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println(">> main begin");
+        System.out.println();
+
+        testCode();
+
+        System.out.println();
+        System.out.println(">> main end");
+        System.out.println();
+    }
+
+    private static void testCode() {
+        System.err.println("testCode execute");
+    }
+}
