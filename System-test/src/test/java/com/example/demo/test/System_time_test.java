8b0950dbc46cb717dd3cf08a008da5713698d173
hothoony <hothoony@hanmail.net>
2025-02-11 23:00:30 +0900

    .

--- System-test/src/test/java/com/example/demo/test/System_time_test.java
+++ System-test/src/test/java/com/example/demo/test/System_time_test.java
@@ -11,13 +11,20 @@ public class System_time_test {
 
     @Test
     void test1() {
+//        1. 1739282396207
+//        2. 1739282396209
+//        3. 1739282396
+//        4. 1739282396209
+//        5. 1739282396
+//        6. 1739282396219
+//        7. 56576490121916
         System.out.println("1. " + (new Date()).getTime());
         System.out.println("2. " + System.currentTimeMillis());
         System.out.println("3. " + Instant.now().getEpochSecond());
         System.out.println("4. " + Instant.now().toEpochMilli());
-        System.out.println("5. " + System.nanoTime());
-        System.out.println("6. " + LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond());
-        System.out.println("7. " + LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+        System.out.println("5. " + LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond());
+        System.out.println("6. " + LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+        System.out.println("7. " + System.nanoTime());
     }
 
     @Test
@@ -27,9 +34,9 @@ public class System_time_test {
 //            System.out.println("2. " + System.currentTimeMillis()); // 중복 있음
 //            System.out.println("3. " + Instant.now().getEpochSecond()); // 중복 있음
 //            System.out.println("4. " + Instant.now().toEpochMilli()); // 중복 있음
-            System.out.println("5. " + System.nanoTime()); // 중복 없음 => O
-//            System.out.println("6. " + LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond()); // 중복 있음
-//            System.out.println("7. " + LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()); // 중복 있음
+//            System.out.println("5. " + LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond()); // 중복 있음
+//            System.out.println("6. " + LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()); // 중복 있음
+            System.out.println("7. " + System.nanoTime()); // 중복 없음 => O
         }
     }
 }
