a29debc77e79eb2256ff98412b3976bfdcca45ad
hothoony <hothoony@hanmail.net>
2025-04-02 21:43:40 +0900

    .

---
+++ file-test/src/test/java/org/study/filetest/FileTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.study.filetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ file-test/src/test/java/org/study/filetest/file_test/file_test.java
@@ -0,0 +1,49 @@
+package org.study.filetest.file_test;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class file_test {
+
+    @Test
+    void test1() {
+        String userHome = System.getProperty("user.home");
+        System.out.println("userHome = " + userHome);
+    }
+
+    @Test
+    void fileExist_io() {
+
+        String BASE_DIR = System.getProperty("user.home") + "/uploads/";
+        String filePath = BASE_DIR + "2025/04/01/voice_70900-1743461466231.wav";
+        System.out.println("filePath = " + filePath);
+
+        boolean isFileExist = isFileExist_withIO(filePath);
+        System.out.println("isFileExist = " + isFileExist);
+    }
+
+    @Test
+    void fileExist_nio() {
+
+        String BASE_DIR = System.getProperty("user.home") + "/uploads/";
+        String filePath = BASE_DIR + "2025/04/01/voice_70900-1743461466231.wav";
+        System.out.println("filePath = " + filePath);
+
+        boolean isFileExist = isFileExist_withNIO(filePath);
+        System.out.println("isFileExist = " + isFileExist);
+    }
+
+    public boolean isFileExist_withIO(String filePath) {
+        File file = new File(filePath);
+        return file.exists();
+    }
+
+    public boolean isFileExist_withNIO(String filePath) {
+        Path path = Paths.get(filePath);
+        return Files.isReadable(path);
+    }
+}
