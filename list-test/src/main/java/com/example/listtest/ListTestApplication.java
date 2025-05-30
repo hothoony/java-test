545ef4ebf9914ef35b30cc2764461769fcbce38f
hothoony <hothoony@hanmail.net>
2025-05-30 11:22:08 +0900

    .

---
+++ list-test/src/main/java/com/example/listtest/ListTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.listtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListTestApplication.class, args);
+    }
+
+}
---
+++ list-test/src/test/java/com/example/listtest/test1/ListTest.java
@@ -0,0 +1,58 @@
+package com.example.listtest.test1;
+
+import lombok.Data;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+
+    @Test
+    void test1() {
+
+        List<MemberDto> memberList = new ArrayList<>();
+        memberList.add(new MemberDto("회원 A"));
+        memberList.add(new MemberDto("회원 B"));
+        memberList.add(new MemberDto("회원 C"));
+        printList(memberList);
+
+        updateMobile(memberList);
+        printList(memberList);
+
+        updateNickname(memberList);
+        printList(memberList);
+    }
+
+    private void updateNickname(List<MemberDto> memberList) {
+        for (MemberDto memberDto : memberList) {
+            memberDto.setMemberNickname("사용자 별명 1");
+        }
+    }
+
+    private void updateMobile(List<MemberDto> memberList) {
+        for (MemberDto memberDto : memberList) {
+            memberDto.setMobilePhone("010-1234-5678");
+        }
+    }
+
+    private void printList(List<MemberDto> memberList) {
+        System.out.println();
+        for (MemberDto memberDto : memberList) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    @Data
+    static public class MemberDto {
+        private Long memberId;
+        private String memberNm;
+        private String memberEmail;
+        private String memberNickname;
+        private String mobilePhone;
+
+        public MemberDto(String memberNm) {
+            this.memberNm = memberNm;
+        }
+    }
+}
