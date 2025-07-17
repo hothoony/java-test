7bc91c5dce6f94304737d9a83acc78184faebe54
hothoony <hothoony@hanmail.net>
2025-07-17 17:04:38 +0900

    .

--- emoji-test/src/test/java/com/example/demo/emoji_test/emoji_4byte_test.java
+++ emoji-test/src/test/java/com/example/demo/emoji_test/emoji_4byte_test.java
@@ -1,6 +1,21 @@
 package com.example.demo.emoji_test;
 
+import com.example.demo.EmojiDto;
+import org.junit.jupiter.api.Test;
+
 public class emoji_4byte_test {
+
+    @Test
+    void removeEmoji_test() {
+
+        for (String emojiString : EmojiDto.emojiStringList) {
+            String removeResult = remove4ByteChars(emojiString);
+            System.out.println();
+            System.out.println("before : " + emojiString);
+            System.out.println("after  : " + removeResult);
+//            assertThat(removeResult).isEmpty();
+        }
+    }
 
     public static String remove4ByteChars(String str) {
         if (str == null) return null;
