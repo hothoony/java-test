312bff7a6ae573c29423a915cbd15e500ba13308
hothoony <hothoony@hanmail.net>
2025-07-18 17:08:16 +0900

    .

--- emoji-test/src/test/java/com/example/demo/emoji_test/emoji_4byte_test.java
+++ emoji-test/src/test/java/com/example/demo/emoji_test/emoji_4byte_test.java
@@ -1,5 +1,6 @@
 package com.example.demo.emoji_test;
 
+import com.example.demo.utils.EmojiUtil;
 import org.junit.jupiter.api.Test;
 
 public class emoji_4byte_test {
@@ -60,38 +61,11 @@ public class emoji_4byte_test {
         String[] list = emojiStringList;
 
         for (String emojiString : list) {
-            String removeResult = remove4ByteChars(emojiString);
+            String removeResult = EmojiUtil.remove4ByteChars(emojiString);
             System.out.println();
             System.out.println("before : " + emojiString);
             System.out.println("after  : " + removeResult);
 //            assertThat(removeResult).isEmpty();
         }
-    }
-
-    // * mariadb
-    //   - charset: utf-8
-    //   - collate: utf8-general-ci
-    // 에서는 3바이트 까지만 저장 가능하므로
-    // 4바이트 이상 문자는 제거한다
-    public static String remove4ByteChars(String str) {
-        if (str == null) return null;
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < str.length(); ) {
-            int codePoint = str.codePointAt(i);
-
-            // U+0000 ~ U+FFFF : BMP (Basic Multilingual Plane) 영역까지만 허용 (UTF-8 최대 3바이트)
-            if (codePoint <= 0xFFFF) {
-                System.out.println("codePoint 포함 = " + codePoint);
-                sb.appendCodePoint(codePoint);
-            } else {
-                System.err.println("codePoint 제거 = " + codePoint);
-            }
-
-            i += Character.charCount(codePoint);
-        }
-
-        return sb.toString();
     }
 }
