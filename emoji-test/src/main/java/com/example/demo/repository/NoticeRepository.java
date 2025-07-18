663bb1ef8668149e4c79dda6eb990437f99d28cd
hothoony <hothoony@hanmail.net>
2025-07-18 15:01:29 +0900

    .

--- emoji-test/src/main/java/com/example/demo/repository/NoticeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.NoticeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<NoticeEntity, Long> {
-}
