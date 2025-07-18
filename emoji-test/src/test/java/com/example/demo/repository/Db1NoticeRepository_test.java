24255d8582eb8165346c12a4dd64fc0b878e6467
hothoony <hothoony@hanmail.net>
2025-07-18 17:01:09 +0900

    .

--- emoji-test/src/test/java/com/example/demo/repository/Db1NoticeRepository_test.java
+++ emoji-test/src/test/java/com/example/demo/repository/Db1NoticeRepository_test.java
@@ -24,7 +24,7 @@ class Db1NoticeRepository_test {
         String emoji3Bytes = "DB 공지사항 😀😂😍😢😎 이모지 테스트"; // 3바이트 이모지
         String emoji4Bytes = "DB 공지사항 👨‍👩‍👧‍👦🧑🏽🧑‍💻🏳️‍🌈🧑‍ 이모지 테스트"; // 4바이트 이모지
 
-        String str = emoji3Bytes;
+        String str = emoji4Bytes;
 
         // given
         NoticeEntity noticeEntity = new NoticeEntity();
